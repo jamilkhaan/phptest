ac24b74afa36063f759d1d94021090397c5bebea
jamilkhan <engrjameelkhan@gmail.com>
2016-02-22 07:02:28 -0800

    Added Login page to the project

---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Admin Panel</title>
+        <link rel="stylesheet" href="../contents/assets/bootstrap/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-sm-offset-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+                                Login
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Javascripts -->
+        <script type="text/javascript" src="../contents/assets/plugins/jquery-1.11.3.min.js"></script>
+    </body>
+</html>
