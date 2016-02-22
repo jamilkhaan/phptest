e2eb15f42ff7b2bf05a8a227b1548177dcf3e2fd
jamilkhan <engrjameelkhan@gmail.com>
2016-02-21 23:09:48 -0800

    Added project configuration fiel

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_HOST', 'localhost');
+
+define('DB_NAME', '');
+
+define('DB_USER', '');
+
+define('DB_PASS', '');
+
+define('SITEURL', 'http://localhost/phptest');
+
+define('ABSPATH', dirname(__FILE__).'/');
+
+echo ABSPATH;
