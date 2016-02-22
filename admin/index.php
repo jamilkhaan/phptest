b3bef40de8215632b4d14e187e1e49e473e3420e
jamilkhan <engrjameelkhan@gmail.com>
2016-02-22 06:37:20 -0800

    Added index file to admin folder

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Admin Panel</title>
+        <link rel="stylesheet" href="../contents/assets/bootstrap/css/bootstrap.min.css.map">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <!-- Javascripts -->
+        <script type="text/javascript" src="../contents/assets/plugins/jquery-1.11.3.min.js"></script>
+    </body>
+</html>
